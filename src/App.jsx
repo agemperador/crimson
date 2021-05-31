441d26bd35d9ae8d854b2b3db9049cc5d2969d48
agemperador <emperadoragustin@gmail.com>
2021-05-31 15:57:00 -0300

    crimson studio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( 
+    <div className = "App" >
+        <header className = "App-header" >
+            <img src = { logo }
+            className = "App-logo"
+            alt = "logo" / >
+            <p> PÁGINA EN CONSTRUCCIÓN </p> 
+            <p> CRIMSON ESTUDIO GRÁFICO </p> 
+        </header>
+    </div>
+    );
+}
+
+export default App
